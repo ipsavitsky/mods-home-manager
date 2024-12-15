dc2401cdefa9acc9b2e12586c016e5414c81b10a
Ilya Savitsky <ipsavitsky234@gmail.com>
2024-12-15 14:20:56 +0000

    Move to a flake structure

--- flake.nix
+++ flake.nix
@@ -1,65 +1,7 @@
 {
-  config,
-  lib,
-  pkgs,
-  ...
-}:
-
-with lib;
-let
-  cfg = config.programs.mods;
-  yamlFormat = pkgs.formats.yaml { };
-in
-{
-  options.programs.mods = {
-    enable = mkEnableOption "mods";
-
-    package = mkOption {
-      type = types.package;
-      default = pkgs.mods;
+  outputs = {self}:{
+    homeManagerModules = {
+      mods = import ./mods.nix;
     };
-
-    settings = mkOption {
-      type = yamlFormat.type;
-      default = { };
-    };
-
-    enableBashIntegration = mkOption {
-      default = false;
-    };
-
-    enableZshIntegration = mkOption {
-      default = false;
-    };
-
-    enableFishIntegration = mkOption {
-      default = false;
-    };
-  };
-
-  config = {
-    home.packages = [ cfg.package ];
-
-    xdg.configFile."mods/mods.yml" = mkIf (cfg.settings != { }) {
-      source = yamlFormat.generate "mods.yml" cfg.settings;
-    };
-
-    programs.bash.initExtra = mkIf cfg.enableBashIntegration (
-      mkOrder 200 ''
-        source <(mods completion bash)
-      ''
-    );
-
-    programs.zsh.initExtra = mkIf cfg.enableZshIntegration (
-      mkOrder 200 ''
-        source <(mods completion zsh)
-      ''
-    );
-
-    programs.fish.interactiveShellInit = mkIf cfg.enableFishIntegration (
-      mkOrder 200 ''
-        mods completion fish | source
-      ''
-    );
   };
 }
