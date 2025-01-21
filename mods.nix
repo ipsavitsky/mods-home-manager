574bd69d76724ec055f2284e0ba24d067caedd2a
Ilya Savitsky <ilya.savitsky@codethink.co.uk>
2025-01-21 11:21:34 +0000

    Backport changes from home-manager pr
    Signed-off-by: Ilya Savitsky <ilya.savitsky@codethink.co.uk>

--- mods.nix
+++ mods.nix
@@ -17,27 +17,50 @@ in
     package = mkOption {
       type = types.package;
       default = pkgs.mods;
+      defaultText = literalExpression "pkgs.mods";
+      description = "The mods package to install";
     };
 
     settings = mkOption {
       type = yamlFormat.type;
       default = { };
+      example = ''
+        {
+          default-model = "llama3.2";
+          apis = {
+            ollama = {
+              base-url = "http://localhost:11434/api";
+              models = {
+                "llama3.2" = {
+                  max-input-chars = 650000;
+                };
+              };
+            };
+          };
+        }
+      '';
+      description = ''
+        Configuration written to
+        {file}`$XDG_CONFIG_HOME/mods/mods.yml`.
+        See <https://github.com/charmbracelet/mods/blob/main/config_template.yml> for the full
+        list of options.
+      '';
     };
 
-    enableBashIntegration = mkOption {
+    enableBashIntegration = mkEnableOption "Bash integration" // {
       default = false;
     };
 
-    enableZshIntegration = mkOption {
+    enableZshIntegration = mkEnableOption "Zsh integration" // {
       default = false;
     };
 
-    enableFishIntegration = mkOption {
+    enableFishIntegration = mkEnableOption "Fish integration" // {
       default = false;
     };
   };
 
-  config = {
+  config = mkIf cfg.enable {
     home.packages = [ cfg.package ];
 
     xdg.configFile."mods/mods.yml" = mkIf (cfg.settings != { }) {
@@ -46,19 +69,19 @@ in
 
     programs.bash.initExtra = mkIf cfg.enableBashIntegration (
       mkOrder 200 ''
-        source <(mods completion bash)
+        source <(${pkgs.mods}/bin/mods completion bash)
       ''
     );
 
     programs.zsh.initExtra = mkIf cfg.enableZshIntegration (
       mkOrder 200 ''
-        source <(mods completion zsh)
+        source <(${pkgs.mods}/bin/mods completion zsh)
       ''
     );
 
     programs.fish.interactiveShellInit = mkIf cfg.enableFishIntegration (
       mkOrder 200 ''
-        mods completion fish | source
+        ${pkgs.mods}/bin/mods completion fish | source
       ''
     );
   };
